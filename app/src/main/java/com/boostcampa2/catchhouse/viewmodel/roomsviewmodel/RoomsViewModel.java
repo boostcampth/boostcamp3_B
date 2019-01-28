7495e9df6e8e53885392f233cd3cc22a6176f8be
pengin7384 <pengin7384@gmail.com>
2019-01-29 01:12:09 +0900

    RoomsViewModel과 RoomsViewModel의 Factory 추가

---
+++ app/src/main/java/com/boostcampa2/catchhouse/viewmodel/roomsviewmodel/RoomsViewModel.java
@@ -0,0 +1,23 @@
+package com.boostcampa2.catchhouse.viewmodel.roomsviewmodel;
+
+import android.app.Application;
+
+import com.boostcampa2.catchhouse.data.roomsdata.RoomsRepository;
+import com.boostcampa2.catchhouse.data.roomsdata.pojo.Room;
+import com.boostcampa2.catchhouse.viewmodel.ReactiveViewModel;
+import com.boostcampa2.catchhouse.viewmodel.ViewModelListener;
+
+import java.util.List;
+
+public class RoomsViewModel extends ReactiveViewModel {
+    private Application mAppContext;
+    private RoomsRepository mRepository;
+    private ViewModelListener mListener;
+    private List<Room> mRoomList;
+
+    RoomsViewModel(Application application, RoomsRepository repository, ViewModelListener listener) {
+        super();
+
+    }
+
+}
