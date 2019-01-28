9add9052e1e87af9a605ab535ef8c07d904b139e
sabgilhun <sabgilhun@gmail.com>
2019-01-28 17:53:12 +0900

    modify value-name & add databinding

--- app/src/main/java/com/boostcampa2/catchhouse/view/fragments/HomeFragment.java
+++ app/src/main/java/com/boostcampa2/catchhouse/view/fragments/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.boostcampa2.catchhouse.view.fragments;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,16 +10,15 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.boostcampa2.catchhouse.R;
+import com.boostcampa2.catchhouse.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     public static final String TAG = HomeFragment.class.getName();
 
-    private TextView mSearchButton;
-    private TextView mPostArticleButton;
+    private FragmentHomeBinding mBinding;
     private OnSearchButtonListener mListener;
 
     @Nullable
@@ -26,18 +26,17 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
+
+        return mBinding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        mSearchButton = view.findViewById(R.id.search_btn);
-        mSearchButton.setOnClickListener(__ -> mListener.onClicked());
-
-        mPostArticleButton = view.findViewById(R.id.post_article_btn);
-        mPostArticleButton.setOnClickListener(__ -> Log.i(TAG, "start write activity"));
+        mBinding.tvHomeSearch.setOnClickListener(__ -> mListener.onClicked());
+        mBinding.tvHomePost.setOnClickListener(__ -> Log.i(TAG, "start write activity"));
     }
 
     @Override
@@ -49,4 +48,5 @@ public class HomeFragment extends Fragment {
     public interface OnSearchButtonListener {
         void onClicked();
     }
+
 }
