d2cc8de1166c074d930f288d885452350a5fcad1
pengin7384 <pengin7384@gmail.com>
2019-01-31 18:21:29 +0900

    AddressBindingAdapter 구성

---
+++ app/src/main/java/com/swsnack/catchhouse/data/roomsdata/pojo/Address.java
@@ -0,0 +1,47 @@
+package com.swsnack.catchhouse.data.roomsdata.pojo;
+
+public class Address {
+    private String name;
+    private String address;
+    private double longitude;
+    private double latitude;
+
+    public Address(String name, String address, double longitude, double latitude) {
+        this.name = name;
+        this.address = address;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+}
