90df82e8131885b25348b6d4dd0609afd6769997
pengin7384 <pengin7384@gmail.com>
2019-01-31 18:22:06 +0900

    View, VIewModel 검색기능 구현
    ViewModel에 SearchAddress 메소드 추가
    EditText를 2way databinding하여 처리
    MapFragment 이벤트 처리

--- app/src/main/java/com/swsnack/catchhouse/viewmodel/roomsviewmodel/RoomsViewModel.java
+++ app/src/main/java/com/swsnack/catchhouse/viewmodel/roomsviewmodel/RoomsViewModel.java
@@ -1,22 +1,68 @@
 package com.swsnack.catchhouse.viewmodel.roomsviewmodel;
 
 import android.app.Application;
+import android.arch.lifecycle.MutableLiveData;
+import android.util.Log;
+import android.view.View;
 
+import com.skt.Tmap.TMapPOIItem;
+import com.skt.Tmap.TMapPoint;
 import com.swsnack.catchhouse.data.roomsdata.RoomsRepository;
+import com.swsnack.catchhouse.data.roomsdata.pojo.Address;
 import com.swsnack.catchhouse.viewmodel.ReactiveViewModel;
 import com.swsnack.catchhouse.viewmodel.ViewModelListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class RoomsViewModel extends ReactiveViewModel {
     private Application mAppContext;
     private RoomsRepository mRepository;
     private ViewModelListener mListener;
-    // private List<Room> mRoomList;
+    private MutableLiveData<List<Address>> mAddressList;
+    public MutableLiveData<String> mKeyword;
 
     RoomsViewModel(Application application, RoomsRepository repository, ViewModelListener listener) {
         super();
         mAppContext = application;
         mRepository = repository;
         mListener = listener;
+        mAddressList = new MutableLiveData<>();
+        mKeyword = new MutableLiveData<>();
+
+        List<Address> list = new ArrayList<>();
+
+        mAddressList.postValue(list);
+    }
+
+    public MutableLiveData<List<Address>> getAddressList() {
+        return mAddressList;
+    }
+
+
+    public void searchAddress() {
+
+        mListener.isWorking();
+        getCompositeDisposable().add(mRepository.getPOIFromRemote(mKeyword.getValue())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(tMapPOIItems -> {
+                    List<Address> tempList = new ArrayList<>();
+                    for(int i = 0; i < tMapPOIItems.size(); i++) {
+                        TMapPOIItem item = tMapPOIItems.get(i);
+                        tempList.add(new Address(item.name, item.getPOIAddress().replace("null", ""), item.getPOIPoint().getLongitude(), item.getPOIPoint().getLatitude()));
+                    }
+                    mAddressList.postValue(tempList);
+                    mListener.isFinished();
+                }, throwable -> {
+                    Log.d("csh", "Error:" + throwable.getMessage());
+                    mListener.isFinished();
+                }
+        ));
+        Log.v("csh", "key:"+mKeyword.getValue().toString());
     }
 
 }
