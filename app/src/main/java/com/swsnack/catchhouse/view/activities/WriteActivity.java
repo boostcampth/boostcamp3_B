b3e0171e768c7f0cf93faa74167f426e2e7acc4b
sabgilhun <sabgilhun@gmail.com>
2019-02-03 16:02:09 +0900

    add datepicker

--- app/src/main/java/com/swsnack/catchhouse/view/activities/WriteActivity.java
+++ app/src/main/java/com/swsnack/catchhouse/view/activities/WriteActivity.java
@@ -1,5 +1,6 @@
 package com.swsnack.catchhouse.view.activities;
 
+import android.app.DatePickerDialog;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.ClipData;
 import android.content.Intent;
@@ -7,7 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
-import android.widget.Toolbar;
+import android.widget.TextView;
 
 import com.swsnack.catchhouse.R;
 import com.swsnack.catchhouse.data.roomsdata.RoomsRepository;
@@ -18,13 +19,21 @@ import com.swsnack.catchhouse.viewmodel.ViewModelListener;
 import com.swsnack.catchhouse.viewmodel.roomsviewmodel.RoomsViewModel;
 import com.swsnack.catchhouse.viewmodel.roomsviewmodel.RoomsViewModelFactory;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
 
 public class WriteActivity extends BaseActivity<ActivityWriteBinding> implements ViewModelListener {
 
     private static final String TAG = WriteActivity.class.getName();
     final int PICK_IMAGE_MULTIPLE = 1;
     private RoomsViewModel mViewModel;
+    private Calendar currentDate;
 
     @Override
     protected int setLayout() {
@@ -73,7 +82,12 @@ public class WriteActivity extends BaseActivity<ActivityWriteBinding> implements
             intent.setAction(Intent.ACTION_GET_CONTENT);
             startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_MULTIPLE);
         });
+
+        currentDate = new GregorianCalendar(Locale.KOREA);
+        getBinding().tvWriteDateFrom.setOnClickListener(v -> createDatePicker((TextView) v));
+        getBinding().tvWriteDateTo.setOnClickListener(v -> createDatePicker((TextView) v));
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -103,4 +117,32 @@ public class WriteActivity extends BaseActivity<ActivityWriteBinding> implements
     private void createViewModels() {
         createViewModel(RoomsViewModel.class, new RoomsViewModelFactory(getApplication(), RoomsRepository.getInstance(), this));
     }
+
+    private void createDatePicker(TextView view) {
+        DatePickerDialog dialog = new DatePickerDialog(this, (__, y, m, d) ->
+                view.setText(y + "-" + m + "-" + d),
+                currentDate.get(Calendar.YEAR),
+                currentDate.get(Calendar.MONTH),
+                currentDate.get(Calendar.DAY_OF_MONTH));
+        dialog.show();
+    }
+
+    public int doDiffOfDate(String from, String to) {
+
+        int diffDays = -1;
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            Date beginDate = formatter.parse(from);
+            Date endDate = formatter.parse(to);
+
+            long diff = endDate.getTime() - beginDate.getTime();
+            diffDays = (int) (diff / (24 * 60 * 60 * 1000));
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return diffDays;
+    }
+
 }
