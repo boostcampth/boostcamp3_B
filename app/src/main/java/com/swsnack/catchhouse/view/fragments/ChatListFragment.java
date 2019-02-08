6e88d506ea9dd59dfc450cc8a720b1c0e1531c27
leeyh <gch02310@naver.com>
2019-02-09 01:15:12 +0900

    feature/C-46
    chattingList UI 작업

--- app/src/main/java/com/swsnack/catchhouse/view/fragments/ChatListFragment.java
+++ app/src/main/java/com/swsnack/catchhouse/view/fragments/ChatListFragment.java
@@ -1,10 +1,8 @@
 package com.swsnack.catchhouse.view.fragments;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
