e488079bc05b5327a9267b00cac45cbcf5d1a86b
sabgilhun <sabgilhun@gmail.com>
2019-02-11 13:10:14 +0900

    add postlayout and view, viewmodel

---
+++ app/src/main/java/com/swsnack/catchhouse/view/activities/PostActivity.java
@@ -0,0 +1,48 @@
+package com.swsnack.catchhouse.view.activities;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.swsnack.catchhouse.R;
+import com.swsnack.catchhouse.data.AppDataManager;
+import com.swsnack.catchhouse.data.chattingdata.remote.RemoteChattingManager;
+import com.swsnack.catchhouse.data.userdata.api.AppAPIManager;
+import com.swsnack.catchhouse.data.userdata.remote.AppUserDataManager;
+import com.swsnack.catchhouse.databinding.ActivityPostBinding;
+import com.swsnack.catchhouse.view.BaseActivity;
+import com.swsnack.catchhouse.viewmodel.postviewmodel.PostViewModel;
+import com.swsnack.catchhouse.viewmodel.postviewmodel.PostViewModelFactory;
+
+public class PostActivity extends BaseActivity<ActivityPostBinding> implements PostListener {
+
+    private PostViewModel mViewModel;
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_post;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        createViewModels();
+        mViewModel = ViewModelProviders.of(this).get(PostViewModel.class);
+        getBinding().setHandler(mViewModel);
+        getBinding().setLifecycleOwner(this);
+    }
+
+    private void createViewModels() {
+        createViewModel(PostViewModel.class,
+                new PostViewModelFactory(
+                        AppDataManager.getInstance(
+                                AppAPIManager.getInstance(),
+                                AppUserDataManager.getInstance(),
+                                RemoteChattingManager.getInstance()
+                        ),
+                        this
+                )
+        );
+    }
+}
