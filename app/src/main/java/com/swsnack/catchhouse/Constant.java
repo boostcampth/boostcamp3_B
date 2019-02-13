5031966af86dda8291dfabcf3b5b5faed087caf9
pengin7384 <pengin7384@gmail.com>
2019-02-13 22:26:58 +0900

    기타 설정

--- app/src/main/java/com/swsnack/catchhouse/Constant.java
+++ app/src/main/java/com/swsnack/catchhouse/Constant.java
@@ -58,6 +58,7 @@ public class Constant {
     public static final int GALLERY = 1003;
     public static final int GOOGLE_SIGN_IN = 1000;
     public static final int FILTER = 1005;
+    public static final String INTENT_FILTER = "1006";
 
     public static final String MSG_ERROR_GET_ADDRESS = "주소를 찾을 수 없습니다.";
     public static final String DEFAULT_DATE_STRING = "YYYY-MM-DD";
